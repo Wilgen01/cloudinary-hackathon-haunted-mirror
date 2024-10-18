979c4f862fc78d0777306ac713eb53f29645f9e9
Wilgen Garcia Rodriguez <wilgengarcia2001@gmail.com>
2024-10-18 12:05:37 -0500

    feat: :sparkles: agrega servicio y componente de timer

---
+++ src/app/shared/components/timer/timer.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, inject, Input, OnChanges, Output, Signal, SimpleChanges } from '@angular/core';
+import { TimerService } from '../../services/timer.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-timer',
+  standalone: true,
+  imports: [],
+  templateUrl: './timer.component.html',
+  styleUrl: './timer.component.scss'
+})
+export class TimerComponent implements OnChanges{
+  @Input({ required: true }) time: number = 0;
+  @Output() timeEnd: EventEmitter<void> = new EventEmitter();
+  private readonly timerService: TimerService = inject(TimerService);
+
+  timer: Signal<string> = this.timerService.timer;
+
+  constructor() {
+    this.timerService.timeEnd$.pipe(takeUntilDestroyed()).subscribe(() => {
+      this.timeEnd.emit();
+    });
+  }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    changes['time'].currentValue && this.timerService.startTimer(changes['time'].currentValue);
+  }
+}
---
+++ src/app/shared/services/timer.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, signal, WritableSignal } from '@angular/core';
+import { interval, Subject, take, timer } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimerService {
+  private timeEnd: Subject<void> = new Subject();
+  timer: WritableSignal<string> = signal<string>('00:30:00');
+  timeEnd$ = this.timeEnd.asObservable();
+  
+
+  startTimer(ms: number) {
+    this.setTimer(ms);
+    interval(1000).pipe(
+      take(Math.ceil(ms / 1000))
+    ).subscribe({
+      next: (sec) => this.setTimer(ms - (sec * 1000)),
+      complete: () => this.endTimer()
+    })
+  }
+
+  private setTimer(ms: number) {
+    const seconds = Math.floor((ms / 1000) % 60);
+    const minutes = Math.floor((ms / (1000 * 60)) % 60);
+    const hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
+
+    const hoursStr = (hours < 10) ? "0" + hours : hours;
+    const minutesStr = (minutes < 10) ? "0" + minutes : minutes;
+    const secondsStr = (seconds < 10) ? "0" + seconds : seconds;
+
+    this.timer.set(`${hoursStr}:${minutesStr}:${secondsStr}`);
+  }
+
+  private endTimer() {
+    timer(1000).subscribe(() => {
+      this.timer.set('00:00:00');
+      this.timeEnd.next();
+    });
+  }
+
+}
