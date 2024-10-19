0050415994a8657e73895d256eaa11e4f72f76b9
Wilgen Garcia Rodriguez <wilgengarcia2001@gmail.com>
2024-10-18 23:41:02 -0500

    feat: :sparkles: agrga toggle pagina resultados

--- src/app/game/components/result/result.component.ts
+++ src/app/game/components/result/result.component.ts
@@ -11,25 +11,30 @@ import { byRadius } from '@cloudinary/url-gen/actions/roundCorners';
 import { source } from '@cloudinary/url-gen/actions/overlay';
 import { image } from '@cloudinary/url-gen/qualifiers/source';
 import { Position } from '@cloudinary/url-gen/qualifiers';
+import { CloudinaryService } from '../../../shared/services/cloudinary.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-result',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './result.component.html',
   styleUrl: './result.component.scss'
 })
 export class ResultComponent implements OnInit {
   private readonly activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  private readonly cloudinaryService: CloudinaryService = inject(CloudinaryService);
 
   cld: Cloudinary = cloudinaryConf;
   img: CloudinaryImage;
   imgBase: CloudinaryImage;
   imageId: string = 'haunted_mirror/ufs6rea5v2kqo28eckio';
+  tieneMarco: boolean = true;
 
   ngOnInit(): void {
     this.obtenerImageId();
-    this.generarImagenes();
+    this.imgBase = this.generarImagenBase();
+    this.img = this.generarImagenMarco(); 
   }
 
   obtenerImageId() {
@@ -38,10 +43,6 @@ export class ResultComponent implements OnInit {
     });
   }
 
-  generarImagenes() {
-    this.imgBase = this.cld.image(this.imageId);
-    this.img = this.generarImagenBase();
-  }
 
   generarImagenBase() {
     const img = this.cld.image(this.imageId);
@@ -56,7 +57,31 @@ export class ResultComponent implements OnInit {
       generativeReplace().from('face').to('scary demon face with horns on the head sharp teeth and red eyes from which blood flows')
     ).effect(
       generativeBackgroundReplace().prompt('terrifying background infested with demons seeking to trap a soul')
-    ).resize(scale().width(500).height(500))
+    )
+    return img;
+  }
+
+  downloadImage() {
+    this.cloudinaryService.warmImage(this.img.toURL()).subscribe({
+      next: (res) => {
+        const blob = new Blob([res], { type: 'image/jpeg' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'coverImage.jpg';
+        a.click();
+        window.URL.revokeObjectURL(url);
+      }
+    })
+  }
+
+  toggleMarco() {
+    this.img = this.tieneMarco ? this.generarImagenMarco() : this.generarImagenBase();
+  }
+
+  generarImagenMarco() {
+    let img = this.generarImagenBase();
+    img.resize(scale().width(500).height(500))
       .resize(auto().aspectRatio("8:10"))
       .roundCorners(byRadius(200))
       .overlay(
@@ -69,4 +94,5 @@ export class ResultComponent implements OnInit {
 
     return img;
   }
+
 }
