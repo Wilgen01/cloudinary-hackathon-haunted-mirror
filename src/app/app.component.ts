96f0ea4c5251717d4c0d76b3c01644e9ac7f4cf1
Wilgen Garcia Rodriguez <wilgengarcia2001@gmail.com>
2024-10-28 08:36:59 -0500

    feat: :page_facing_up: Agrega redireccion a github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,13 @@ import { initFlowbite } from 'flowbite';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { CharacterComponent } from './character/character.component';
 import { ControlsComponent } from './shared/components/controls/controls.component';
+import { GithubComponent } from './shared/components/github/github.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CloudinaryModule, CharacterComponent, ControlsComponent, NgxSpinnerModule],
+  imports: [CommonModule, RouterOutlet, CloudinaryModule, CharacterComponent, ControlsComponent, NgxSpinnerModule, GithubComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
